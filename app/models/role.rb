43d48f8aad2b063f77de84d03856e7f111094f4b
leonwong03 <108467768+leonwong03@users.noreply.github.com>
2022-09-29 05:57:49 -0400

    commit tree 7c34ffaec2bdaff1aead446c66e0e4e492144012

---
+++ app/models/role.rb
@@ -0,0 +1,29 @@
+class Role < ActiveRecord::Base
+
+    has_many :auditions
+
+    def actors
+        self.auditions.pluck(:actor)
+    end
+
+    def locations
+        self.auditions.pluck(:location)
+    end
+
+    def lead
+        audition = self.auditions.find{|audition| audition.hired} 
+        if audition
+            audition
+        else 
+            "no actor has been hired for this role"
+        end
+    end
+
+    def understudy 
+        auditions = self.auditions.filter{|audition| audition.hired}
+        if auditions[1]
+            auditions[1]
+        else
+            "no actor has been hired for understudy for this role"
+        end
+    end
